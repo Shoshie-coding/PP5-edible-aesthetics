80f8c72d885b7782a2a9648e731e7fbac0093c9e
Cristina Onea <cristinaonea86@gmail.com>
2022-07-02 11:33:16 +0000

    Customize admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,11 +1,28 @@
 from django.contrib import admin
 from .models import Product, Category
 
-# Register your models here
+# Class shows product details on the Admin Panel
+# Based on Code Institute's Boutique Ado project
 
-admin.site.register(Product)
-admin.site.register(Category)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
 
+    ordering = ('sku',)
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
 
 
