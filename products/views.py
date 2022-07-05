fd5b8c3b4d6cd2084c378c8d970f6f774479adb0
Cristina Onea <cristinaonea86@gmail.com>
2022-07-05 18:26:55 +0000

    Add bag context and delivery logic

--- products/views.py
+++ products/views.py
@@ -7,7 +7,9 @@ from .models import Product, Category
 
 # Create your views here.
 
+
 def all_products(request):
+
     """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
