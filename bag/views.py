567a9e9ff7c4511aa23d93c53c678dc180605b21
Cristina Onea <cristinaonea86@gmail.com>
2022-07-08 17:02:19 +0000

    Update toast_success template

--- bag/views.py
+++ bag/views.py
@@ -74,11 +74,15 @@ def adjust_bag(request, item_id):
             bag.pop(item_id)
             messages.success(request, f'Removed {product.name} from your bag')
 
+    request.session['bag'] = bag	
+    return redirect(reverse('view_bag'))
+
 
 def remove_from_bag(request, item_id):
     """Remove the item from the shopping bag"""
 
     try:
+        product = get_object_or_404(Product, pk=item_id)	
         size = None
         if 'product_size' in request.POST:
             size = request.POST['product_size']
@@ -88,8 +92,11 @@ def remove_from_bag(request, item_id):
             del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
+            messages.success(request, f'Removed size {size.upper()} {product.name} from your bag')	
         else:
             bag.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your bag')	
+
  
         request.session['bag'] = bag
         return HttpResponse(status=200)
