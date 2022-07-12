3f417e371db0452b17451424f8e0659d920c5c7c
Cristina Onea <cristinaonea86@gmail.com>
2022-07-12 21:04:48 +0000

    Set to get secret key from environ and set debug to true

--- aesthetics/settings.py
+++ aesthetics/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+3va-7ybf*03z^d(sm^p7^4jqxryc9+los5nhu58&-ehq5ck!j'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['photo-aesthetics.herokuapp.com', 'localhost']
 
