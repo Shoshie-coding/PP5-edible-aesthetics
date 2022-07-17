4e3af5688a6fef910b86002249dba79d977e6c71
Cristina Onea <cristinaonea86@gmail.com>
2022-07-17 22:08:03 +0000

    Add js script in index to fix access to product management after signing in

--- home/views.py
+++ home/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from products.models import *
 
+
 # Create your views here.
 def index(request):
     """ A view to return the index page """
     popular_products = Product.objects.filter(count__gt=0).order_by('count')[:5]
-     
+    
     return render(request, 'home/index.html', {'popular_products': popular_products})
 
- 
+
 def page_not_found_view(request, exception):
     return render(request, 'home/404.html', status=404)
