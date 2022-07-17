20f60b9c12b1e805e6ec451c9f8535dbbd5194f2
Cristina Onea <cristinaonea86@gmail.com>
2022-07-17 13:37:11 +0000

    Remove product_form, create forms.py and update views and urls files for product admin in products app

--- products/urls.py
+++ products/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
-from django.contrib.admin.views.decorators import staff_member_required
+# from django.contrib.admin.views.decorators import staff_member_required
 
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('create', staff_member_required(views.ProductCreateView.as_view()), name="create"),
-    path('delete', views.delete, name='delete'),
-    path('edit/<pk>', staff_member_required(views.ProductUpdateView.as_view()), name="edit"), 
-    path('<product_id>', views.product_detail, name='product_detail'),
+    # path('create', staff_member_required(views.ProductCreateView.as_view()), name="create"),
+    # path('delete', views.delete, name='delete'),
+    # path('edit/<pk>', staff_member_required(views.ProductUpdateView.as_view()), name="edit"), 
+    path('<int:product_id>/', views.product_detail, name='product_detail'),
+    path('add/', views.add_product, name='add_product'),
+
 ]
--- products/views.py
+++ products/views.py
@@ -2,32 +2,16 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
-from django.views.generic.edit import CreateView, UpdateView
+
 from .models import Product, Category
-from django.contrib.admin.views.decorators import staff_member_required
+from .forms import ProductForm
 
 # Create your views here.
-class ProductCreateView(CreateView):
-    model = Product
-    fields = '__all__'   
-
-class ProductUpdateView(UpdateView):
-    model = Product
-    fields = '__all__'   
-
-@staff_member_required
-def delete(request):
-    if request.method == 'POST': 
-
-        p = Product.objects.get(pk=request.POST['id'])
-        p.delete()
 
 def all_products(request):
-
     """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
-    popular_products = Product.objects.filter(count__gt=0).order_by('count')[:5]
     query = None
     categories = None
     sort = None
@@ -47,6 +31,7 @@ def all_products(request):
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -55,19 +40,16 @@ def all_products(request):
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(
-                    request, "You didn't enter any search criteria!")
+                messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
             
-            queries = Q(name__icontains=query) | Q(
-                description__icontains=query)
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
     current_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
-        'popular_products': popular_products,
         'search_term': query,
         'current_categories': categories,
         'current_sorting': current_sorting,
@@ -86,3 +68,16 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
+
+    
