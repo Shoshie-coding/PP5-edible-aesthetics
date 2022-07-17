ea2589536e8711865440f8553a069b47b4f2831d
Cristina Onea <cristinaonea86@gmail.com>
2022-07-17 16:45:59 +0000

    Remove test images from media folder

--- products/urls.py
+++ products/urls.py
@@ -4,10 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
-    # path('create', staff_member_required(views.ProductCreateView.as_view()), name="create"),
-    # path('delete', views.delete, name='delete'),
     path('<int:product_id>/', views.product_detail, name='product_detail'),
     path('add/', views.add_product, name='add_product'),
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
+    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
 
 ]
